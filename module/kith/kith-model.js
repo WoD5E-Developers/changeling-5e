282f77c5e1919b584eb32a0691e43d8b5deb1705
Veilza <113429647+Veilza@users.noreply.github.com>
2026-01-04 17:14:52 -0600

    Convert to extending new WOD5E data models + various adjustments

--- module/kith/kith-model.js
+++ module/kith/kith-model.js
@@ -1,43 +1,6 @@
-export class KithModel extends foundry.abstract.TypeDataModel {
+export class KithModel extends WOD5E.WoDItemModel {
   static defineSchema() {
-    const fields = foundry.data.fields
-    const schema = {}
-
-    schema.description = new fields.HTMLField({
-      initial: '',
-      required: false,
-      blank: true
-    })
-
-    schema.affinity = new fields.StringField({
-      initial: '',
-      required: false,
-      blank: true
-    })
-
-    schema.gamesystem = new fields.StringField({
-      initial: 'changeling',
-      required: true,
-      blank: false
-    })
-
-    schema.source = new fields.SchemaField(
-      {
-        book: new fields.StringField({
-          required: false,
-          blank: true,
-          initial: ''
-        }),
-        page: new fields.StringField({
-          required: false,
-          blank: true,
-          initial: ''
-        })
-      },
-      {
-        label: 'changeling-5e.Notes'
-      }
-    )
+    const schema = super.defineSchema()
 
     return schema
   }
